d127c2d873303d7cfd6e2128ed69a7c71d0415b2
Dayvison <dayvisondapazpereira@outlook.com>
2022-11-03 10:30:45 -0300

    create project

--- back/index.js
+++ back/index.js
@@ -1,2 +1,16 @@
+const express = require("express")
+const cors = require("cors")
+const jwt = require("jsonwebtoken")
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
 
-console.log("ola")
+const app = express()
+app.use(cors())
+
+
+
+app.get("/",(req,res)=>{
+res.send("hello world")
+})
+
+app.listen(3000,console.log('server on'))
